ede99ead46c37abf2e86400b0bcd5e724a8d6b3e
zubokoff <alezubokoff@gmail.com>
2020-01-16 23:57:37 -0300

    first commit

---
+++ src/main/java/br/com/zubokoff/apieditora/ApieditoraApplication.java
@@ -0,0 +1,13 @@
+package br.com.zubokoff.apieditora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApieditoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApieditoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zubokoff/apieditora/ApieditoraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zubokoff.apieditora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApieditoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
